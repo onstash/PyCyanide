ad225282b35e2143313c5cd90b5a09fc75504ce4
Santosh Venkatraman <santosh.venk@gmail.com>
2017-02-22 01:00:08 +0530

    Adds cleaned flow to download comics directly

--- pycyanide.py
+++ pycyanide.py
@@ -1,9 +1,110 @@
+"""Script to download Cyanide & Happiness comics."""
+__author__ = "Santosh Venkatraman<santosh.venk@gmail.com>"
+from os import path, getcwd, mkdir, makedirs
 import sys
 import time
+import logging
+from PIL import Image
+from StringIO import StringIO
+
+from dateutil.parser import parse as date_parse
+from lxml.etree import HTML
+from requests import get
+from requests.exceptions import ReadTimeout, ConnectionError, \
+    ConnectTimeout
 
 
+BASE_URL = "http://explosm.net/comics/latest"
+COMICS_DIR = path.join(getcwd(), "comics")
+if not path.exists(COMICS_DIR):
+    try:
+        mkdir(COMICS_DIR)
+    except BaseException:
+        makedirs(COMICS_DIR)
 
 
+def get_tree(url):
+    """Helper method to get Tree of the url provided."""
+    try:
+        return HTML(get(url, timeout=3).content)
+    except (ReadTimeout, ConnectTimeout, ConnectionError) as error:        
+        logging.error(error)
+
+
+def fetch_data(url):
+    """Helper method to fetch data from url."""
+    page_tree = get_tree(url)
+    if page_tree is None:
+        return
+    image_link = page_tree.xpath("//img[@id='main-comic']/@src")[0]
+    image_link = "http:{}".format(image_link) \
+        if image_link.startswith("//") else \
+        image_link
+    permalink = page_tree.xpath("//input[@id='permalink']/@value")[0]
+    date = \
+        page_tree.xpath("//div[@class='meta-data']/div/h3/a/text()")[0]
+    date = date_parse(date)
+    author = page_tree.xpath(
+        "//small[@class='author-credit-name']/text()"
+    )[0].strip("by ")
+    comic_number = permalink.strip("/").split("/")[-1]
+    return {
+        "number": comic_number,
+        "image": image_link,
+        "permalink": permalink,
+        "metadata": {
+            "date": (date.year, date.month, date.day),
+            "author": author
+        }
+    }
+
+
+def generate_comic_link(number):
+    """Helper method to generate comic link based on number."""
+    return "http://explosm.net/comics/{number}".format(**locals())
+
+
+def process_comic(url):
+    download_comic(fetch_data(url))
+
+
+def download_comic(data):
+    """Helper method to download comic from source to destination."""
+    if not data:
+        return
+    date = data.get("metadata").get("date")
+    destination = path.join(COMICS_DIR,
+                            str(date[0]),
+                            str(date[1]),
+                            str(date[2]))
+    if not path.exists(destination):
+        makedirs(destination)
+    destination = \
+        path.join(destination, "{}.png".format(data.get("number")))
+    if path.exists(destination):
+        print("Already downloaded comic - {destination}".format(
+            **locals())
+        )
+        return
+    response = get(data.get("image"), stream=True)
+    Image.open(StringIO(response.content)).save(destination)
+    print("Downloaded comic - {destination}".format(**locals()))
 
 
 if __name__ == '__main__':
+    root_page = fetch_data(BASE_URL)
+    if not root_page:
+        sys.exit("Internet connection is ded!")
+    latest_comic = int(root_page.get("number"))
+    comic_links = map(generate_comic_link, xrange(latest_comic, -1, -1))
+    for url in comic_links:
+        try:
+            process_comic(url)
+        except KeyboardInterrupt:
+            sys.exit()
+        except BaseException as error:
+            print error
+            _traceback, _value, _type = sys.exc_info()
+            print _traceback
+            print _value
+            print _type
